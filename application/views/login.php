fbc960604f637268e9a7296f8a2f1d8ad8536f2b
Vivek Unnikrishnan <vivekhtc25@gmail.com>
2014-11-22 15:10:45 +0530

    Changes to login and register form, added htaccess file for url hiding

--- application/views/login.php
+++ application/views/login.php
@@ -12,12 +12,12 @@ $this->load->helper('url');
   
   <title>Login</title>  
   <link  href="http://fonts.googleapis.com/css?family=Cabin:400,500,600,bold" rel="stylesheet" type="text/css" >
-
-<link  href="http://fonts.googleapis.com/css?family=PT+Sans+Narrow:regular,bold" rel="stylesheet" type="text/css" >
-  <link rel="stylesheet" href="http://localhost/spreeci/lib/onsen/css/onsenui.css">  
-  <link rel="stylesheet" href="http://localhost/spreeci/lib/onsen/css/onsen-css-components.css">  
+  <link href="http://localhost/spreeci/assets/css/bootstrap.css" rel="stylesheet">
+  <link href="http://localhost/spreeci/assets/css/font-awesome.min.css" rel="stylesheet">
+  <link  href="http://fonts.googleapis.com/css?family=PT+Sans+Narrow:regular,bold" rel="stylesheet" type="text/css" >
+   
   <link rel="stylesheet" href="http://localhost/spreeci/styles/app.css"/>
-<link rel="stylesheet" href="http://localhost/spreeci/css/main.css"/>
+  <link rel="stylesheet" href="http://localhost/spreeci/css/main.css"/>
   <script src="lib/onsen/js/angular/angular.js"></script>    
   <script src="lib/onsen/js/onsenui.js"></script>   
   <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script> 
@@ -41,25 +41,46 @@ function submitFormWithValue(){
   </div>
   <div id="icon" align="center" style=" padding-top:200px;"><img src="http://localhost/spreeci/img/car icon2.png")?>
   </div>
-  <div id="loginform" style="padding-top:50px; display:none" align="center" >
-  <h1 style="font-size:24px;">Login</h1>
+  <div class="container" id="loginform" style="display:none">
+    <div class="row">
+    <div class="col-md-4 col-md-offset-4">
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            <h3 class="panel-title">Login</h3>
+        </div>
+          <div class="panel-body">
   <?php
   echo form_open('','');
+  echo "  <fieldset> <div class='form-group'>";
   echo form_label($login_form["email"]["label"], $login_form["email"]["field"]);
-  echo form_input($login_form["email"]["field"],'');
+  $data = array('name' => $login_form["email"]["field"],
+                 'class'=> "form-control",
+                  'placeholder'=> "Email" );
+  echo form_input($data);
   echo "<br><br>";
-
+  
   echo form_label($login_form["password"]["label"], $login_form["password"]["field"]);
-  echo form_password($login_form["password"]["field"],'');
+  $data['name']=$login_form["password"]["field"];
+  $data['placeholder']="Password";
+  echo form_password($data);
   echo "<br><br>";
-  echo form_submit('submit','Submit');
+  echo "</div>";
+  $datasub= array('name' => "submit" ,
+                   'value' => "Login",
+                    'class' => "btn btn-lg btn-success btn-block" );
+  echo form_submit($datasub);
+  echo "</fieldset>";
   echo form_close();
   ?>
-  <br><br>
-  <a href="http://localhost/spreeci/index.php/login/register"class="buttonsuper">
-        <span>Register</span>
+  <br>
+    <button type="button" class="btn btn-lg btn-warning btn-block" onclick="window.location.href='http://localhost/spreeci/login/register'">Register</button>
     </a>
   </div>
+  </div>
+  </div>
+  </div>
+  </div>
+  
   
   <script>
     setTimeout(fade_out, 10000);
--- application/views/register.php
+++ application/views/register.php
@@ -2,58 +2,74 @@
 <head>
   <meta charset="utf-8">
   <meta name="apple-mobile-web-app-capable" content="yes">
-  <meta name="mobile-web-app-capable" content="yes">
-  
+  <meta name="mobile-web-app-capable" content="yes"> 
   <title>Register</title>  
   <link  href="http://fonts.googleapis.com/css?family=Cabin:400,500,600,bold" rel="stylesheet" type="text/css" >
-
-<link  href="http://fonts.googleapis.com/css?family=PT+Sans+Narrow:regular,bold" rel="stylesheet" type="text/css" >
+  <link  href="http://fonts.googleapis.com/css?family=PT+Sans+Narrow:regular,bold" rel="stylesheet" type="text/css" >
   <link rel="stylesheet" href="http://localhost/spreeci/styles/app.css"/>
-<link rel="stylesheet" href="http://localhost/spreeci/css/main.css"/>
+  <link rel="stylesheet" href="http://localhost/spreeci/css/main.css"/>
   <script src="lib/onsen/js/angular/angular.js"></script>    
   <script src="lib/onsen/js/onsenui.js"></script>   
+  <link href="http://localhost/spreeci/assets/css/bootstrap.css" rel="stylesheet">
+  <link href="http://localhost/spreeci/assets/css/font-awesome.min.css" rel="stylesheet">
   <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script> 
 
   <script src="http://localhost/spreeci/cordova.js"></script>
-  <script src="http://localhost/spreeci/app.js"></script> 
-  
-
-  
-  
+  <script src="http://localhost/spreeci/app.js"></script>   
 </head>
 
 <body background="http://localhost/spreeci/img/Drag Layer.png")?>
- 
- <div style="padding-top: 50px;" align="center">
- <h1 style="font-size:24px;">Register</h1>
+<div class="container" style="padding-top:70px;">
+    <div class="row">
+    <div class="col-md-4 col-md-offset-4">
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            <h3 class="panel-title" align="center"><b>Register</b></h3>
+        </div>
+          <div class="panel-body">
 <?php
   echo form_open('','');
-  echo "Hello";
-  echo "<br><br>";
-  echo form_label($register_form["email"]["label"], $register_form["email"]["field"]);
-  echo form_input($register_form["email"]["field"],'');
-  echo "<br><br>";
-  echo form_label($register_form["password"]["label"], $register_form["password"]["field"]);
-  echo form_input($register_form["password"]["field"],'');
-  echo "<br><br>";
-  echo form_label($register_form["conpassword"]["label"], $register_form["conpassword"]["field"]);
-  echo form_input($register_form["conpassword"]["field"],'');
-  echo "<br><br>";
-  echo form_label($register_form["name"]["label"], $register_form["name"]["field"]);
-  echo form_input($register_form["name"]["field"],'');
-  echo "<br><br>";
-  echo form_label($register_form["telno"]["label"], $register_form["telno"]["field"]);
-  echo form_input($register_form["telno"]["field"],'');
-  echo "<br><br>";
-  echo form_label($register_form["collegename"]["label"], $register_form["collegename"]["field"]);
-  echo form_input($register_form["collegename"]["field"],'');
+  echo "  <fieldset> <div class='form-group'>";
+  
+  $data = array('name' => $register_form["email"]["field"],
+                 'class'=> "form-control",
+                  'placeholder'=> "Email" );
+  echo form_input($data);
+  echo "<br>";
+  $data['name']=$register_form["password"]["field"];
+  $data['placeholder']="Password";
+  echo form_password($data);
+  echo "<br>";
+  $data['name']=$register_form["conpassword"]["field"];
+  $data['placeholder']="Confirm Password";
+  echo form_password($data);
+  echo "<br>";
+  $data['name']=$register_form["name"]["field"];
+  $data['placeholder']="Name";
+  echo form_input($data);
+  echo "<br>";
+  $data['name']=$register_form["telno"]["field"];
+  $data['placeholder']="Telephone";
+  echo form_input($data);
+  echo "<br>";
+  $data['name']=$register_form["collegename"]["field"];
+  $data['placeholder']="College";
+  echo form_input($data);
 
-  echo "<br><br>";
-  echo form_submit('submit','Submit');
+  echo "<br>";
+  $datasub= array('name' => "submit" ,
+                   'value' => "Register",
+                    'class' => "btn btn-lg btn-warning btn-block" );
+  echo form_submit($datasub);
+  echo "</fieldset>";
   echo form_close();
 
 ?>
-</div>
+  </div>
+  </div>
+  </div>
+  </div>
+  </div>
 
 </body>
 </html>
